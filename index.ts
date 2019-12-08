7a622ff4e5953902ca934515a6d67ec3b447a524
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-12-08 15:50:56 +0000

    added xero html to json converter

--- index.ts
+++ index.ts
@@ -1,51 +1,62 @@
 import { readFile } from 'fs'
-import * as htmlparser2 from 'htmlparser2'
 import * as files from './files.json'
 
-const file = (<any>files).file
+interface XeroAccountTransaction {
+    date: string
+    description: string
+    reference: string
+    paymentRef: string
+    spent: string
+    received: string
+    bankTransactionSource: string
+    status: string
+}
 
-readFile(file, 'utf8', (error, data) => {
-    let records = 0
-    let transactions = []
-    let openTdTag = false
-    let openTrTag = false
-    let transaction = []
-    let firstRow = true
+const Transform = (x: string[]): XeroAccountTransaction => {
+    return  {
+                date: x[1],
+                description: x[2],
+                reference: x[3],
+                paymentRef: x[4],
+                spent: x[5],
+                received: x[6],
+                bankTransactionSource: x[7],
+                status: x[8].replace('\n', '').trim()
+            }
+}
 
-    const parser = new htmlparser2.Parser({
-        onopentag(name, attribs) {
-            if (name === 'tr') {
-                records += 1
-                openTrTag = true
-            }
-            if (name === 'td') {
-                openTdTag = true
-            }
-        },
-        ontext(text) {
-            if(openTrTag && openTdTag && !firstRow) {
-                transaction = [...transaction, text]
-            }
-        },
-        onclosetag(tagname) {
-            if (tagname === 'tr') {
-                openTrTag = false
-                if (firstRow) {
-                    firstRow = false
-                } else {
-                    transactions = [...transactions, transaction];
-                    transaction = [];
-                }
-            }
+export function htmlToJson(): Promise<any> {
+    return new Promise((resolve, reject) => {
+        const file = (<any>files).file
 
-            if (tagname === 'td') {
-                openTdTag = false
-            }
-        }
-    },
-        { decodeEntities: true });
+        readFile(file, 'utf8', (error, data) => {
+            const cheerio = require('cheerio')
+            const $ = cheerio.load(data)
+            const elements = $('tbody > tr')
+            let element = elements.first()
+            const xeroAccountTransactions = [];
 
-    parser.write(data)
-    parser.end()
-    console.log(`${records}`, `${transactions.length}, ${transactions[0][0]}, ${transactions[184][0]}`)
-})
+            while(element.html() != null) {
+                const values = $('td', element)
+                xeroAccountTransactions.push(Transform([
+                    $(values[0]).text(),
+                    $(values[1]).text(),
+                    $(values[2]).text(),
+                    $(values[3]).text(),
+                    $(values[4]).text(),
+                    $(values[5]).text(),
+                    $(values[6]).text(),
+                    $(values[7]).text(),
+                    $(values[8]).text(),
+                    $(values[9]).text()
+                ]))
+                element = element.next()
+            }
+            resolve(xeroAccountTransactions)
+        })
+    })
+}
+
+(async () => {
+    console.log(await htmlToJson())
+})()
